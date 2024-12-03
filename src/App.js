c34e7b3a86b166797bb9f00b8917d8037ee078ed
Carmen Cruz Arana <carmenrubi.arana@gmail.com>
2024-12-03 17:45:09 -0500

    Linked login page to landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
 import HomePage from './pages/HomePage';
 // Remove or comment out authentication imports
-// import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/LoginPage';
 // import RegisterPage from './pages/RegisterPage';
 // import PrivateRoute from './components/PrivateRoute';
 
@@ -21,8 +21,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<HomePage />} />
           {/* Remove or comment out authentication routes */}
-          {/* <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} /> */}
+          <Route path="/login" element={<LoginPage />} />
+          {/*<Route path="/register" element={<RegisterPage />} /> */}
           {/* Fallback Route */}
           <Route path="*" element={<LandingPage />} />
         </Routes>
